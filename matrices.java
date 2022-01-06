ca0178d2d56a4bf42db9bd8da4214a0c6d30ded1
Clisman <kibar_623@hotmail.com>
2022-01-05 20:10:22 -0500

    matriz

---
+++ matrices.java
@@ -0,0 +1,22 @@
+public class Matrices{
+	 
+
+	
+	public static void main(String[] args){
+		
+		
+		int [][]matriz= new int[5][5];
+		
+		
+		for(int i=0; i<matriz.length; i++){
+			for(int j=0; j<matriz.length; j++){
+					System.out.println(matriz[i][j]);
+			}
+			
+			System.out.println("\n");
+		}
+	}
+}
+		
+
+	
---
+++ prueba.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php 
+	
+for($i=0; $i<3 ; $i++) {
+ 	for($j=0; $j<3; $j++){
+ 		echo rand(0,9);
+ 	}
+ 	echo "<br>";
+}
+ ?>
+		
+</body>
+</html>
